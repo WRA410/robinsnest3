085187dc81f8d1539080814f09ce9ef35d1b74a4
Grant McCarty <mccar396@msu.edu>
2016-12-06 20:37:34 -0500

    Structure Redesign
    Messages got moved to home page
    Messages tab removed
    Members tab needs to be fixed as well as Home address rework
    Home page done as far as can be without high fidelity wireframes
    Messages themselves seem to be working properly
    Title needs work, but once again need wireframes
    Need to redesign login/signup/logout
    Would like Individual profile page but need to rework Members and
    addressings
    -Maybe edit Edit Profile too
    Funtion mostly working, need to work on styling

--- members.php
+++ members.php
@@ -1,69 +1,87 @@
-<?php // Example 26-9: members.php
+<?php // Example 26-11: messages.php
   require_once 'header.php';
 
   if (!$loggedin) die();
 
-  echo "<div class='main'>";
+  if (isset($_GET['view'])) $view = sanitizeString($_GET['view']);
+  else                      $view = $user;
 
-  if (isset($_GET['view']))
+  if (isset($_POST['text']))
   {
-    $view = sanitizeString($_GET['view']);
-    
-    if ($view == $user) $name = "Your";
-    else                $name = "$view's";
-    
-    echo "<h3>$name Profile</h3>";
-    showProfile($view);
-    echo "<a class='button' href='messages.php?view=$view'>" .
-         "View $name messages</a><br><br>";
-    die("</div></body></html>");
+    $text = sanitizeString($_POST['text']);
+
+    if ($text != "")
+    {
+      $pm   = 0;
+      $time = time();
+      queryMysql("INSERT INTO messages VALUES(NULL, '$user',
+        '$view', '$pm', $time, '$text')");
+    }
   }
 
-  if (isset($_GET['add']))
+  if ($view != "")
   {
-    $add = sanitizeString($_GET['add']);
+    if ($view == $user) $name1 = $name2 = "Your";
+    else
+    {
+      $name1 = "<a href='members.php?view=$view'>$view</a>'s";
+      $name2 = "$view's";
+    }
 
-    $result = queryMysql("SELECT * FROM friends WHERE user='$add' AND friend='$user'");
-    if (!$result->num_rows)
-      queryMysql("INSERT INTO friends VALUES ('$add', '$user')");
-  }
-  elseif (isset($_GET['remove']))
-  {
-    $remove = sanitizeString($_GET['remove']);
-    queryMysql("DELETE FROM friends WHERE user='$remove' AND friend='$user'");
-  }
-
-  $result = queryMysql("SELECT user FROM members ORDER BY user");
-  $num    = $result->num_rows;
-
-  echo "<h3>Other Members</h3><ul>";
-
-  for ($j = 0 ; $j < $num ; ++$j)
-  {
-    $row = $result->fetch_array(MYSQLI_ASSOC);
-    if ($row['user'] == $user) continue;
+    echo "<div class='main'>";
+    // <h3>$name1 Home</h3>";
+    // showProfile($view);
     
-    echo "<li><a href='members.php?view=" .
-      $row['user'] . "'>" . $row['user'] . "</a>";
-    $follow = "follow";
+    echo <<<_END
+      <form method='post' action='members.php?view=$view'>
+      Type here to leave a message:<br>
+      <textarea name='text' cols='40' rows='3'></textarea><br>
+      <input type='submit' value='Post Message'></form><br>
+_END;
 
-    $result1 = queryMysql("SELECT * FROM friends WHERE
-      user='" . $row['user'] . "' AND friend='$user'");
-    $t1      = $result1->num_rows;
-    $result1 = queryMysql("SELECT * FROM friends WHERE
-      user='$user' AND friend='" . $row['user'] . "'");
-    $t2      = $result1->num_rows;
-
-    if (($t1 + $t2) > 1) echo " &harr; is a mutual friend";
-    elseif ($t1)         echo " &larr; you are following";
-    elseif ($t2)       { echo " &rarr; is following you";
-      $follow = "recip"; }
+    if (isset($_GET['erase']))
+    {
+      $erase = sanitizeString($_GET['erase']);
+      queryMysql("DELETE FROM messages WHERE id=$erase");
+    }
     
-    if (!$t1) echo " [<a href='members.php?add="   .$row['user'] . "'>$follow</a>]";
-    else      echo " [<a href='members.php?remove=".$row['user'] . "'>drop</a>]";
+    $query  = "SELECT * FROM messages ORDER BY time DESC";
+    $result = queryMysql($query);
+    $num    = $result->num_rows;
+    
+    for ($j = 0 ; $j < $num ; ++$j)
+    {
+      $row = $result->fetch_array(MYSQLI_ASSOC);
+
+      if ($row['pm'] == 0 || $row['auth'] == $user)
+      {
+        $messenger= $row['auth'];
+        if (file_exists("$messenger.jpg"))
+        echo "<img src='$messenger.jpg' style='width:50px; float:left'>";
+
+        echo "<div style='font-size:12px;'>" ;
+        echo date ('M jS g:ia' , $row['time']);
+        echo "</div>";
+
+        echo " <a href='members.php?view=" . $row['auth'] . "' id='messagestuff'>" . $row['auth']. "</a> :";
+        echo "<br>";
+        echo " &quot;" . $row['message'] . "&quot; ";
+
+
+        if ($row['auth'] == $user)
+          echo "<a href='members.php?view=$view" .
+               "&erase=" . $row['id'] . "' style='font-size:12px; text-decoration:none;'>[delete]</a>";
+        echo "<br>";
+        echo "<br>";
+      }
+    }
   }
+
+  if (!$num) echo "<br><span class='info'>No messages yet</span><br><br>";
+
+  echo "<br><a class='button' href='members.php?view=$view'>Refresh messages</a>";
 ?>
 
-    </ul></div>
+    </div><br>
   </body>
 </html>
