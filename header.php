3c3849d74b9965f5fccc6c4a9329ece6f34f30d9
Grant McCarty <mccar396@msu.edu>
2016-12-12 13:35:00 -0500

    Revert "Just small change in header"
    This reverts commit 7a38422f5605ec1d5e3b42958897b1288fa5aeb5.

--- header.php
+++ header.php
@@ -28,7 +28,7 @@
          "<ul class='menu'>" .
          "<li><a href='members.php?view=$user'>Home</a></li>" .
          "<li><a href='members.php'>Members</a></li>"         .
-         "<li><a href='friends.php'>Otakus</a></li>"         .
+         "<li><a href='friends.php'>Friends</a></li>"         .
          "<li><a href='profile.php'>Edit Profile</a></li>"    .
          "<li><a href='logout.php'>Log out</a></li></ul>" .
          "</nav>";
